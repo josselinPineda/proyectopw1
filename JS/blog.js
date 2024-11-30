a36b4e842ced23d387c3f862a063ab35454a4067
josselinPineda <149841107+josselinPineda@users.noreply.github.com>
2024-11-30 00:44:51 -0600

    cmabios blog

--- JS/blog.js
+++ JS/blog.js
@@ -1,10 +1,10 @@
-// Mostrar más información de los artículos
 const articles = document.querySelectorAll('.articulos article');
 
 articles.forEach((article) => {
+
     const moreText = document.createElement('div');
-    moreText.textContent = "Contenido adicional del artículo...";
-    moreText.style.display = 'none';
+    moreText.textContent = "La Cruz Roja también lleva a cabo campañas educativas y de sensibilización sobre diversos temas de salud, bienestar, y derechos humanos. A través de programas de prevención, buscamos reducir el impacto de los desastres y mejorar la calidad de vida de las comunidades vulnerables.";
+    moreText.style.display = 'none'; // 
     article.appendChild(moreText);
 
     const readMoreLink = document.createElement('a');
@@ -14,13 +14,14 @@ articles.forEach((article) => {
     article.appendChild(readMoreLink);
 
     readMoreLink.addEventListener('click', (e) => {
-        e.preventDefault();
+        e.preventDefault(); 
+
         if (moreText.style.display === 'none') {
             moreText.style.display = 'block';
             readMoreLink.textContent = 'Leer menos';
         } else {
             moreText.style.display = 'none';
-            readMoreLink.textContent = 'Leer más';
+            readMoreLink.textContent = 'Leer más'; 
         }
     });
 });
